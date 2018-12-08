6f5866512ec7547ba393f75db04c55e772619345
97994598069 <43906514+97994598069@users.noreply.github.com>
2018-12-08 12:39:54 +0800

    Update 服务器初始化操作.py

--- Linux/服务器初始化操作.py
+++ Linux/服务器初始化操作.py
@@ -34,6 +34,11 @@ chattr +i /etc/gshadow
 在/etc/profile文件最后加两行：
 USER_IP=`who -u -m | awk '{print $NF}'| sed 's/[()]//g'`
 export HISTTIMEFORMAT="[%F %T][`whoami`][${USER_IP}] "
+或者开启审计功能
+HISTSIZE=1000
+HISTTIMEFORMAT="[%F %T][`whoami`][${USER_IP}] ";export HISTTIMEFORMAT
+export HISTORY_FILE=/var/log/audit.log   ##注意日志
+export PROMPT_COMMAND='{ thisHistID=`history 1|awk "{print \\$1}"`;lastCommand=`history 1| awk "{\\$1=\"\" ;print}"`;user=`id -un`;whoStr=(`who -u am i`);realUser=${whoStr[0]};logMonth=${whoStr[2]};logDay=${whoStr[3]};logTime=${whoStr[4]};pid=${whoStr[6]};ip=${whoStr[7]};if [ ${thisHistID}x != ${lastHistID}x ];then echo -E `date "+%Y/%m/%d %H:%M:%S"` $user\($realUser\)@$ip[PID:$pid][LOGIN:$logMonth $logDay $logTime] --- $lastCommand ;lastHistID=$thisHistID;fi; } >> $HISTORY_FILE'
 
 source /etc/profile
 
