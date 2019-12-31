08955c0d03544c157f2ddb13c71fb1f83d259644
97994598069 <43906514+97994598069@users.noreply.github.com>
2019-12-31 13:25:29 +0800

    Update 服务器初始化操作.py

--- Linux/服务器初始化操作.py
+++ Linux/服务器初始化操作.py
@@ -95,4 +95,9 @@ net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_tw_recycle = 1  ##系统bug，可造成网站不可预知的访问异常
 net.ipv4.tcp_fin_timeout = 30
 
+net.ipv4.tcp_syncookies = 1表示开启SYN Cookies。当出现SYN等待队列溢出时，启用cookies来处理，可防范少量SYN攻击，默认为0，表示关闭；
+net.ipv4.tcp_tw_reuse = 1表示开启重用。允许将TIME-WAIT sockets重新用于新的TCP连接，默认为0，表示关闭；
+net.ipv4.tcp_tw_recycle = 1表示开启TCP连接中TIME-WAIT sockets的快速回收，默认为0，表示关闭。
+net.ipv4.tcp_fin_timeout修改系統默认的TIMEOUT时间
+修改之后，再用命令查看TIME_WAIT连接数netstat -ant |grep “TIME_WAIT” |wc –l
 
